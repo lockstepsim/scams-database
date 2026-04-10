481422eba0c83d3aafc2c585d29cd5fd6e8ca1fc
ngomayling <ngomayling@gmail.com>
2026-04-10 22:46:02 +0200

    add scam_channel

--- scams/models.py
+++ scams/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
-SCAM_TYPES = [
-    ("phishing", "Phishing"),
-    ("sms", "SMS Scam"),
-    ("investment", "Investment Scam"),
+SCAM_CHANNELS = [
+    ("email", "Email"),
+    ("sms", "SMS"),
+    ("phone_call", "Phone call"),
+    ("social_media","Social media"),
+    ("in_person","In-person approach"),
+    ("mail","Mail"),
+    ("other","Other channel")
     ]
 
+SCAM_TYPES = [
+    ("financial", "Financial transfer scams"),
+    ("identity", "Identity and impersonation scams"),
+    ("investment", "Investment and wealth scams")
+    ]
 
 class Scam(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    phishing = models.BooleanField(null=True, blank=True)
+    contact_method = models.CharField(max_length=100,choices=SCAM_CHANNELS)
     scam_type = models.CharField(max_length=100,choices=SCAM_TYPES)
     date_seen = models.DateField()
     url_or_contact = models.CharField(max_length=255, blank=True)
