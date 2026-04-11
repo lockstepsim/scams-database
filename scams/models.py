370507202460f566e7af58e1befb1682aa237b36
ngomayling <ngomayling@gmail.com>
2026-04-11 18:21:01 +0200

    modify model and adapt myscript

--- scams/models.py
+++ scams/models.py
@@ -13,14 +13,15 @@ SCAM_CHANNELS = [
 SCAM_TYPES = [
     ("financial", "Financial transfer scams"),
     ("identity", "Identity and impersonation scams"),
-    ("investment", "Investment and wealth scams")
+    ("investment", "Investment and wealth scams"),
+    ("other", "Other scams")
     ]
 
 class Scam(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     phishing = models.BooleanField(null=True, blank=True)
-    contact_method = models.CharField(max_length=100,choices=SCAM_CHANNELS)
+    contact_method = models.CharField(max_length=100,choices=SCAM_CHANNELS, default="other")
     scam_type = models.CharField(max_length=100,choices=SCAM_TYPES)
     date_seen = models.DateField()
     url_or_contact = models.CharField(max_length=255, blank=True)
